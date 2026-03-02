e15ed2f2cefe3a7ddc18fbdd67f6cf690be832e6
seunghyeonL <hkr0416@gmail.com>
2026-03-02 20:23:06 +0900

    [update] 테스트용 MazeSize 5 * 5

--- Source/Maze/GameMode/MazeGameMode.h
+++ Source/Maze/GameMode/MazeGameMode.h
@@ -27,10 +27,10 @@ protected:
 
 	// ---- Maze Configuration ----
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Maze")
-	int32 MazeWidth = 9;
+	int32 MazeWidth = 5;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Maze")
-	int32 MazeHeight = 9;
+	int32 MazeHeight = 5;
 
 	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category="Maze")
 	float CellSize = 500.f;
