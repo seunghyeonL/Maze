0fe1cde26197139b8ea1ad15bb3a15b20f32e8c6
seunghyeonL <hkr0416@gmail.com>
2026-03-18 15:42:15 +0900

    chore: steam setting

--- Source/Maze/UI/MatchWidget.cpp
+++ Source/Maze/UI/MatchWidget.cpp
@@ -176,7 +176,7 @@ void UMatchWidget::HandleCreateLobbyClicked()
 	if (SOSManager)
 	{
 		ShowLoading(FText::FromString(TEXT("세션 생성 중...")));
-		SOSManager->CreateSession(MaxPlayerNum, GetDefault<UMazeLevelSettings>()->GetTitleLevelPath(), true);
+		SOSManager->CreateSession(MaxPlayerNum, GetDefault<UMazeLevelSettings>()->GetTitleLevelPath(), false);
 	}
 	else
 	{
@@ -190,7 +190,7 @@ void UMatchWidget::HandleFindLobbyClicked()
 	if (SOSManager)
 	{
 		ShowLoading(FText::FromString(TEXT("로비 검색 중...")));
-		SOSManager->FindSessions(50, true);
+		SOSManager->FindSessions(50, false);
 	}
 	else
 	{
